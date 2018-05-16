61da3354e419d96446958d671e20e2a53900f34f
nattawoot <spw.ntw@gmail.com>
2018-05-17 01:22:14 +0700

    initial commit for dev

---
+++ src/App.js
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import axios from 'axios';
+import List from './components/List';
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      persons: []
+    }
+  }
+  componentDidMount() {
+    axios.get(`http://uinames.com/api/?ext&amount=25`)
+      .then(res => {
+        const persons = res.data;
+        this.setState({ persons });
+      })
+  }
+  render() {
+    const day = ["SUN","MON","TUE","WED","THU","FRI","SAT"]
+    const content = []
+    for (var i = 0; i < 7; i++) {
+      content.push(
+        <div className="column">
+        <div className="card">
+          <div className="card-content">
+            <p className="subtitle is-center" >
+            {day[i]}
+            </p>
+              {this.state.persons.map((person,index) =>
+               <div key={index}>    
+                  <List person={person} day={day[i]} />
+               </div>  
+              )
+            }  
+          </div>
+        </div>
+        </div>
+      ) 
+    }
+    return (
+      <div className="container">
+        <div className="columns is-gapless">
+          {content}
+        </div>  
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/List.js
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+
+class List extends Component {
+    constructor(props){
+        super(props)
+        this.state = {
+          day : ["SUN","MON","TUE","WED","THU","FRI","SAT"]
+        }
+    } 
+  render() {
+    const content = []
+    const birthday = new Date(this.props.person.birthday.raw)
+    const day = this.props.day
+    const person = this.props.person
+    if (this.state.day[birthday.getDay()] === day) {
+      content.push(
+        <div>
+        <img src={person.photo} alt="golf"/>
+          {person.name}
+        </div>
+      )
+    }
+    return (
+          <div>{content}</div>
+        );
+    }
+}
+export default List;
